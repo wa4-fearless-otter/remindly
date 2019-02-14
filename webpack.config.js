23b7fc913616d35daea79249005380c24234ec3e
Wavum <rl00@gmx.at>
2019-02-14 21:42:07 +0100

    Add basic electron setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require("path");
+const { spawn } = require("child_process");
+const merge = require("webpack-merge");
 const { CheckerPlugin } = require("awesome-typescript-loader");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HardSourceWebpackPlugin = require("hard-source-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackShellPlugin = require("webpack-shell-plugin");
 
-module.exports = {
+const OUT_DIR = path.resolve(__dirname, "dist");
+
+const config = {
   mode: "development",
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[hash].js"
-  },
   devtool: "source-map",
   module: {
     rules: [
@@ -19,10 +21,47 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(OUT_DIR, {
+      verbose: false
+    }),
     new CheckerPlugin(),
-    new HardSourceWebpackPlugin()
+    new HardSourceWebpackPlugin({
+      info: {
+        level: "info",
+      }
+    }),
+    new WebpackShellPlugin({ onBuildEnd: ["electron " + OUT_DIR] })
   ],
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  node: {
+    __dirname: false,
+    __filename: false
   }
 };
+
+module.exports = [
+  merge(config, {
+    target: "electron-main",
+    entry: "./src",
+    output: {
+      path: OUT_DIR,
+      filename: "index.js"
+    }
+  }),
+  merge(config, {
+    ...config,
+    target: "electron-renderer",
+    entry: "./src/renderer",
+    output: {
+      path: path.resolve(OUT_DIR, "renderer/"),
+      filename: "renderer.js"
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/renderer/index.html"
+      })
+    ]
+  })
+]
