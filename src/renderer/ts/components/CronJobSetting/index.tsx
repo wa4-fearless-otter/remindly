8878f8b1037e7268775a17a9175b3743e93a79c7
Wavum <rl00@gmx.at>
2019-03-15 21:15:42 +0100

    Add basic config

--- src/renderer/ts/components/CronJobSetting/index.tsx
+++ src/renderer/ts/components/CronJobSetting/index.tsx
@@ -1,21 +1,28 @@
 import * as React from "react";
+import { Trash2 } from "react-feather";
 
 interface Props extends React.HTMLProps<HTMLDivElement> { }
 
 export default (props: Props) => {
   return (
-    <div className="columns" {...props}>
-      <div className="column">
-        First column
+    <div {...props}>
+      <div className="columns">
+        <div className="column">
+          <input className="input" type="text" placeholder="* * * * *" />
+        </div>
+        <div className="column is-narrow">
+          <button className="button">
+            <span className="icon">
+              <Trash2 />
+            </span>
+          </button>
+        </div>
       </div>
-      <div className="column">
-        Second column
-      </div>
-      <div className="column">
-        Third column
-      </div>
-      <div className="column">
-        Fourth column
+      <div className="select">
+        <select>
+          <option>Select dropdown</option>
+          <option>With options</option>
+        </select>
       </div>
     </div>
   );
--- src/renderer/ts/components/index.tsx
+++ src/renderer/ts/components/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Plus } from "react-feather";
 import CronJobSetting from "./CronJobSetting";
 
 interface Props extends React.HTMLProps<HTMLDivElement> { }
@@ -6,7 +7,19 @@ interface Props extends React.HTMLProps<HTMLDivElement> { }
 export default (props: Props) => {
   return (
     <div {...props}>
-      <CronJobSetting />
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <CronJobSetting />
+
+            <button className="button">
+              <span className="icon">
+                <Plus />
+              </span>
+            </button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
