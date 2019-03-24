7438ad3414b699119e72d1eb3c5102a175e5c45f
Wavum <rl00@gmx.at>
2019-03-24 20:53:34 +0100

    Remove redux

--- src/renderer/ts/index.tsx
+++ src/renderer/ts/index.tsx
@@ -2,4 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render((
+    <App />
+  ), document.getElementById("app"));
